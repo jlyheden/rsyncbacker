95884707034765a4f6429cc6665da4b2c2d306fa
Johan Lyheden <johan@lyheden.com>
2015-08-24 00:25:54 +0200

    fix semantic errors

--- scripts/rsyncbacker_cmd.py
+++ scripts/rsyncbacker_cmd.py
@@ -11,8 +11,10 @@ from rsyncbacker import RsyncExecutor, ConfigurationException
 from rsyncbacker.util import load_config
 
 parser = argparse.ArgumentParser(description="rsyncbacker backup tool.")
-parser.add_argument("-b", "--batch", dest="batch", action="store_true", help="execute in batch mode, without prompts or anything")
-parser.add_argument("--skip-routed", dest="skip_routed", action="store_true", help="don't start backup if on remote network")
+parser.add_argument("-b", "--batch", dest="batch", action="store_true",
+                    help="execute in batch mode, without prompts or anything")
+parser.add_argument("--ignore-lan-check", dest="ignore_lan_check", action="store_true",
+                    help="start backup regardless if target is on same lan as client or not")
 parser.add_argument("config", help="path to configuration yaml")
 
 args = parser.parse_args()
@@ -24,8 +26,9 @@ try:
     executor.commandline_builder()
 except ConfigurationException, ex:
     LOGGER.error(ex)
+    sys.exit(1)
 
-if not args.skip_routed and executor.should_backup_run() is False:
+if not args.ignore_lan_check and executor.should_backup_run() is False:
     LOGGER.info("Refusing to start backup since target is on another network")
     sys.exit(0)
 
